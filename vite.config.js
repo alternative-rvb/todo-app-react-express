03b866a02ff1a5c256c40e8fbbadb386c2adf969
Nicolas <alternative.rvb@gmail.com>
2024-06-10 12:27:43 +0200

    deploy test

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Charger les variables d'environnement en fonction du mode (développement ou production)
   const env = loadEnv(mode, process.cwd());
+
+  // Identifier le fichier .env lu
+  const envFile = mode === 'development' ? '.env' : `.env.${mode}`;
+  console.log(`Loading environment variables from ${envFile}`);
+  
+  // Log the loaded environment variables for verification
+  console.log(env);
 
   return {
     plugins: [react()],
