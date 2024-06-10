a1ca1ace8038215f4d1149a1ab89208b79cf6359
Nicolas <alternative.rvb@gmail.com>
2024-06-10 13:17:23 +0200

    feat: config environnement de production

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,18 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-import fs from 'fs';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  // Charger les variables d'environnement en fonction du mode (développement ou production)
-  const env = loadEnv(mode, process.cwd());
-
-  // Identifier le fichier .env lu
-  const envFile = mode === 'development' ? '.env' : `.env.${mode}`;
-  console.log(`Loading environment variables from ${envFile}`);
-  
-  // Log the loaded environment variables for verification
-  console.log(env);
-
-  return {
-    plugins: [react()],
-    build: {
-      // Chemin où les fichiers de production seront générés
-      outDir: resolve(__dirname, './server/dist'),
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    // Chemin où les fichiers de production seront générés
+    outDir: resolve(__dirname, "./server/dist"),
+  },
+  server: {
+    proxy: {
+      // toutes les requêtes commençant par '/api' seront transférées à 'http://localhost:8080'
+      "/api": "http://localhost:8080",
     },
-    define: {
-      'process.env': env
-    }
-  };
+  },
 });
