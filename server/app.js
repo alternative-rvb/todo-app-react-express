2007309e4a56b3b8416a4a65960a8aceeeed5509
Nicolas <alternative.rvb@gmail.com>
2024-06-12 11:34:23 +0200

    refacto: logs

--- server/app.js
+++ server/app.js
@@ -24,10 +24,12 @@ MongoClient.connect(process.env.MONGO_URI)
 // Logger middleware pour logger chaque requête
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
-  console.log("***", req.body);
+  console.log("↖️  req.body: ")
+  console.log(req.body);
   const oldSend = res.send;
   res.send = function (data) {
-    console.log(`Statut de la réponse: ${res.statusCode}`, data);
+    console.log( '↘️ ', `Status: ${res.statusCode}`);
+    if (data) console.log(JSON.parse(data));
     oldSend.call(this, data);
   }
   next();
